9375a89b07537c0599ef6b64c341a86d101042e1
mjtoolbox <michael.jo@gmail.com>
2022-04-18 11:29:13 -0700

    01 Component + Router navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Component01 from './Component01';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/test' element={<Component01 />} />
+      </Routes>
+    </>
   );
 }
+
+const Navigation = () => {
+  return (
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/test'>Test</Link>
+    </nav>
+  );
+};
 
 export default App;
---
+++ src/Component01.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Component01 = () => {
+  return <h1 style={{ color: 'red' }}>This is a new page</h1>;
+};
+
+export default Component01;
